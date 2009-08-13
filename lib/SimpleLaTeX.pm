1dc0bd4bc58233f73058db1c3142b26f02b6c284
Robin Lee Powell <rlpowell@digitalkingdom.org>
2009-08-13 21:21:04 +0000

    Use the right tmp dir; also, new latex version stuff.

--- lib/SimpleLaTeX.pm
+++ lib/SimpleLaTeX.pm
@@ -56,7 +56,7 @@ sub interpret {
 	$InputText=$OrigInputText;
 
 	# Make a temp directory and file.
-	my $dir = tempdir( DIR => "/home/www/tmp/", CLEANUP => 1 );
+	my $dir = tempdir( DIR => "/var/www/tmp/", CLEANUP => 1 );
 	my ($fh, $filename) = tempfile( DIR => $dir, SUFFIX => ".tex" );
 
 	open(TMP, ">$filename") or return "Couldn't open a temporary file; check your definition for unbalanced dollar signs or other wierd characters.\n";
@@ -83,7 +83,7 @@ sub interpret {
 	close(TMP2);
 
 	# Re-write the image links.
-	$dir =~ s!/home/www!!;
+	$dir =~ s!/var/www/tmp!/tmp!;
 	$tmpout =~ s/.*<BODY[^>]*>//s;
 	$tmpout =~ s/<BR[^>]*><HR[^>]*>.*//s;
 	$tmpout =~ s/SRC="/SRC="http:\/\/www.digitalkingdom.org$dir\//gs;
