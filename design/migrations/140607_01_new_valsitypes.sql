1ea9f7902f3b782c11d92f8e366b026d11bbb1a6
Riley Martinez-Lynch <oss+lojban@teleological.net>
2014-07-22 23:05:24 -0400

    Include zei-lujvo in valsi types

--- design/migrations/140607_01_new_valsitypes.sql
+++ design/migrations/140607_01_new_valsitypes.sql
@@ -7,6 +7,7 @@ INSERT INTO valsitypes (typeId, descriptor) VALUES ( 9, 'bu-letteral');
 INSERT INTO valsitypes (typeId, descriptor) VALUES (10, 'zei-lujvo');
 INSERT INTO valsitypes (typeId, descriptor) VALUES (11, 'obsolete cmene');
 INSERT INTO valsitypes (typeId, descriptor) VALUES (12, 'obsolete fu\'ivla');
+INSERT INTO valsitypes (typeId, descriptor) VALUES (13, 'obsolete zei-lujvo');
 
 COMMIT;
 
