1c912566f078f4689d1de70cf2e78bd711a9717f
Robin Lee Powell <rlpowell@digitalkingdom.org>
2018-05-26 21:19:11 -0700

    Making the file reflect reality in this table.

--- design/jbovlaste.sql
+++ design/jbovlaste.sql
@@ -111,7 +111,9 @@ INSERT INTO valsitypes (typeId, descriptor) VALUES (9, 'bu-letteral');
 INSERT INTO valsitypes (typeId, descriptor) VALUES (10, 'zei-lujvo');
 INSERT INTO valsitypes (typeId, descriptor) VALUES (11, 'obsolete cmevla');
 INSERT INTO valsitypes (typeId, descriptor) VALUES (12, 'obsolete fu\'ivla');
-INSERT INTO valsitypes (typeId, descriptor) VALUES (13, 'phrase');
+INSERT INTO valsitypes (typeId, descriptor) VALUES (13, 'obsolete zei-lujvo');
+INSERT INTO valsitypes (typeId, descriptor) VALUES (14, 'obsolete cmavo');
+INSERT INTO valsitypes (typeId, descriptor) VALUES (15, 'phrase');
 
 -- table of lojban words
 CREATE TABLE valsi (
